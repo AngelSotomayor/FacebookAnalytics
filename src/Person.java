fc0c55d306c1ab6c7bad708e5a120bfb0017aa40
Fabio Fleitas <cubanfabio@gmail.com>
2014-04-23 18:22:03 -0400

    working Reader & Person. provided some junit tests in ReaderTest and small graph sets to test on

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,60 @@
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * 
+ * @author Fabio Fleitas & Shayan Patel
+ *
+ */
 public class Person {
-
+	private int id;
+	private Set<Person> friends;
+	
+	/**
+	 * Initialize Person from id
+	 * @param id
+	 */
+	public Person(int id) {
+		this.id = id;
+		this.friends = new HashSet<Person>();
+	}
+	
+	/**
+	 * Adds person to each other's friend set
+	 * @param p
+	 */
+	public void addFriend(Person p) {
+		if (p == null || friends.contains(p)) { return; }
+		
+		friends.add(p);
+		p.addFriend(this);
+	}
+	
+	/**
+	 * Sets people as friends
+	 * @param people
+	 */
+	public void setFriends(Set<Person> people) {
+		this.friends = people;
+	}
+	
+	/**
+	 * 
+	 * @return Person's id
+	 */
+	public int getId() {
+		return this.id;
+	}
+	
+	/**
+	 * 
+	 * @return Person's friends
+	 */
+	public Set<Person> getFriends() {
+		return this.friends;
+	}
+	
+	public int numberOfFriends() {
+		return this.friends.size();
+	}
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,4 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
+/**
+ * 
+ * @author Fabio Fleitas & Shayan Patel
+ *
+ */
 public class Reader {
+	private HashMap<Integer, Person> people;
 
+	public Reader(String path) {
+		people = new HashMap<Integer, Person>();
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+
+			while (br.ready()) {
+				String line = br.readLine();
+				String[] id = line.split(" ");
+
+				int id0 = Integer.parseInt(id[0]);
+				int id1 = Integer.parseInt(id[1]);
+
+				Person p0 = people.get(id0);
+				Person p1 = people.get(id1);
+
+				if (p0 == null) { 
+					p0 = new Person(id0); 
+					people.put(id0, p0);
+				}
+				if (p1 == null) { 
+					p1 = new Person(id1);
+					people.put(id1, p1);
+				}
+
+				p0.addFriend(p1);
+			}
+
+			br.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFound");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return Person based on id
+	 */
+	public Person getPerson(int id) {
+		return this.people.get(id);
+	}
 }
