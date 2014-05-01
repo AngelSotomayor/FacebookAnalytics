80887b0ea07cae03f3535508a33558411e04a023
Shayan Patel <shayanpatel@hntvlan569.1964.wlan.wireless-resnet.upenn.edu>
2014-05-01 11:03:53 -0400

    MVC design pattern

--- src/ReaderTest.java
+++ src/ReaderTest.java
@@ -1,53 +1,53 @@
-import static org.junit.Assert.*;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import org.junit.Test;
-
-
-public class ReaderTest {
-	
-	@Test
-	public void testSmallInput() {
-		Reader r = new Reader("small_graph.txt");
-		Person p = r.getPerson(0);
-		
-		Set<Person> friends = new HashSet<Person>();
-		friends.add(r.getPerson(1));
-		friends.add(r.getPerson(3));
-		
-		assertEquals(friends, p.getFriends());
-	}
-	
-	@Test
-	public void smallCorrectFriendsSet() {
-		Reader r = new Reader("small_graph.txt");
-		Person p = r.getPerson(2);
-		
-		Set<Person> friends = new HashSet<Person>();
-		friends.add(r.getPerson(1));
-		friends.add(r.getPerson(3));
-		friends.add(r.getPerson(5));
-		
-		assertEquals(friends, p.getFriends());
-	}
-	
-	@Test
-	public void facebookCorrectNumberOfEdges() {
-		Reader r = new Reader("facebook_combined.txt");
-		Person p = r.getPerson(0);
-		
-		assertEquals(347, p.numberOfFriends());
-	}
-	
-	@Test
-	public void facebookContainsFriend() {
-		Reader r = new Reader("facebook_combined.txt");
-		Person p0 = r.getPerson(0);
-		Person p1 = r.getPerson(1);
-		
-		assertTrue(p1.getFriends().contains(p0));
-	}
-	
-}
+//import static org.junit.Assert.*;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//import org.junit.Test;
+//
+//
+//public class ReaderTest {
+//	
+//	@Test
+//	public void testSmallInput() {
+//		Reader r = new Reader("small_graph.txt");
+//		Person p = r.getPerson(0);
+//		
+//		Set<Person> friends = new HashSet<Person>();
+//		friends.add(r.getPerson(1));
+//		friends.add(r.getPerson(3));
+//		
+//		assertEquals(friends, p.getFriends());
+//	}
+//	
+//	@Test
+//	public void smallCorrectFriendsSet() {
+//		Reader r = new Reader("small_graph.txt");
+//		Person p = r.getPerson(2);
+//		
+//		Set<Person> friends = new HashSet<Person>();
+//		friends.add(r.getPerson(1));
+//		friends.add(r.getPerson(3));
+//		friends.add(r.getPerson(5));
+//		
+//		assertEquals(friends, p.getFriends());
+//	}
+//	
+//	@Test
+//	public void facebookCorrectNumberOfEdges() {
+//		Reader r = new Reader("facebook_combined.txt");
+//		Person p = r.getPerson(0);
+//		
+//		assertEquals(347, p.numberOfFriends());
+//	}
+//	
+//	@Test
+//	public void facebookContainsFriend() {
+//		Reader r = new Reader("facebook_combined.txt");
+//		Person p0 = r.getPerson(0);
+//		Person p1 = r.getPerson(1);
+//		
+//		assertTrue(p1.getFriends().contains(p0));
+//	}
+//	
+//}
